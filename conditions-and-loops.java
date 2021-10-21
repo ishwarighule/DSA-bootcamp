5d0904cb7f1080477bdc2dd82d06e2515b39ef4f
Ishwari <87072876+ishwarighule@users.noreply.github.com>
2021-10-21 13:37:13 +0530

    Update conditions-and-loops.java

--- conditions-and-loops.java
+++ conditions-and-loops.java
@@ -51,6 +51,73 @@ public class Main
         
     }}
 
+// reverse
+
+
+public class Main
+{
+    public static void main(String[] args) {
+        int num=232525;
+        int ans=0;
+        
+        while(num>0){
+            
+            int rem=num%10;
+            num=num/10;
+            ans = ans*10 +rem;
+            
+        }
+        
+        
+        System.out.println(ans);
+    }
+}
 
 
 
+//calculator
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) {
+        int ans=0;
+        System.out.println("Enter operator");
+        
+        Scanner sc =new Scanner(System.in);
+        char op =sc.next().trim().charAt(0);
+        
+        while(true){
+            if(op== '+' || op== '-' ||op== '*' ||op== '/' ||op== '%'){
+                 System.out.println("Enter two numbers");
+                 int a = sc.nextInt();
+                 int b= sc.nextInt();
+                 
+                
+                if(op=='+'){
+                    ans=a+b;
+                }
+                if(op=='-'){
+                    ans=a-b;
+                }
+                if(op=='*'){
+                    ans=a*b;
+                }
+                if(op=='/'){
+                    if(b!=0){
+                        ans=a/b;
+                    }
+                 }
+                if(op=='%'){
+                    ans=a%b;
+                }
+            }else if(op=='x'|| op=='X'){
+                break;
+            }
+        else{
+             System.out.println("Invalid operator");
+        }
+        
+        System.out.println(ans);
+    }
+}
+}
