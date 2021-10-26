1b654aed72d44ef12de58b2dd2b797201e898383
Ishwari <87072876+ishwarighule@users.noreply.github.com>
2021-10-26 20:32:26 +0530

    Create Q 4.java

---
+++ Assignments/condition and loops/Q 4.java
@@ -0,0 +1,18 @@
+// Area Of Isosceles Triangle
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        double height,base,area;
+        System.out.print("Enter height: ");
+        height= sc.nextDouble();
+        System.out.print("Enter base: ");
+        base= sc.nextDouble();
+        area= (1 * base * height) / 2;
+        System.out.println("Area of Isosceles Triangle: "+ area);
+    }
+}
