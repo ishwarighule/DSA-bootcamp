2ea9d9dcfca5341eaf102124c8c6d93e695eaeef
Ishwari <87072876+ishwarighule@users.noreply.github.com>
2021-10-27 21:19:42 +0530

    Create Q 13.java

---
+++ Assignments/condition and loops/Q 13.java
@@ -0,0 +1,15 @@
+// Perimeter Of Rhombus
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length");
+        double a = sc.nextDouble();
+        double perimeter ;
+        perimeter = 4 *(a);
+        System.out.println("Perimeter Of Rhombus is " +perimeter);
+    }
+}
