3a19706bb5730fdcc113b7c7f56e2ae9491704a2
Ishwari <87072876+ishwarighule@users.noreply.github.com>
2021-10-27 21:18:30 +0530

    Create Q 12.java

---
+++ Assignments/condition and loops/Q 12.java
@@ -0,0 +1,15 @@
+// Perimeter Of Square
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length");
+        double a = sc.nextDouble();
+        double perimeter ;
+        perimeter = 4 *(a);
+        System.out.println("Perimeter Of Square is " +perimeter);
+    }
+}
